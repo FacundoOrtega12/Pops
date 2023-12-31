5d72c7367185fbd0d06e772e8fc25b1069a574f4
FacundoOrtega12 <fgortega97@gmail.com>
2023-12-30 18:10:07 -0800

    restart project

--- pongv1.py
+++ pongv1.py
@@ -1,6 +1,20 @@
-from draw import Draw
 import random
 import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_ESCAPE,
+    KEYDOWN,
+    K_RETURN,
+    K_q,
+    K_a,
+    QUIT,
+)
+
+
+
+
 class Frame:
     def __init__(self, nce:dict):
         self.nce = nce
@@ -208,4 +222,196 @@ class Frame:
     def move_pad_b_down(self):
         if self.paddle_b_y < self.nce['court_y']:
             self.paddle_b_y = self.paddle_b_y + 20
+
+class Draw:
+    digits = [  #digits written in 4x5 arrays 
+        ((1,1,1,1),(1,0,0,1),(1,0,0,1),(1,0,0,1),(1,1,1,1)), #0
+        ((0,0,0,1),(0,0,0,1),(0,0,0,1),(0,0,0,1),(0,0,0,1)), #1
+        ((1,1,1,1),(0,0,0,1),(1,1,1,1),(1,0,0,0),(1,1,1,1)), #2
+        ((1,1,1,1),(0,0,0,1),(0,0,1,1),(0,0,0,1),(1,1,1,1)), #3
+        ((1,0,0,1),(1,0,0,1),(1,1,1,1),(0,0,0,1),(0,0,0,1)), #4
+        ((1,1,1,1),(1,0,0,0),(1,1,1,1),(0,0,0,1),(1,1,1,1)), #5
+        ((1,1,1,1),(1,0,0,0),(1,1,1,1),(1,0,0,1),(1,1,1,1)), #6
+        ((1,1,1,1),(0,0,0,1),(0,0,0,1),(0,0,0,1),(0,0,0,1)), #7
+        ((1,1,1,1),(1,0,0,1),(1,1,1,1),(1,0,0,1),(1,1,1,1)), #8
+        ((1,1,1,1),(1,0,0,1),(1,1,1,1),(0,0,0,1),(1,1,1,1)), #9
+    ]
+    
+    def __init__(self):
+        self.court = None
+        self.prev_x = 0
+        self.prev_y = 0
+        self.nce = None
+        self.score_a = 0        
+        self.score_b = 0
+        self.prev_pad_a_y = 0
+        self.prev_pad_b_y = 0
+                
+    def init(self, nc_state:dict):
+        pygame.init()
+        self.nce = nc_state
+        self.court = pygame.display.set_mode((nc_state["court_x"], nc_state["court_y"])) 
+        self.pitch()
+
+    def quit(self):
+        pygame.quit()
+
+    def halfcourt_line(self):
+        y_pos = 7
+        x_pos = 412
+        for _ in range(43):
+            pygame.draw.rect(self.court, (255, 255, 255), (x_pos, y_pos, 2, 8))
+            y_pos = y_pos + 12
+
+    def pitch(self):
+        self.court.fill((0, 0, 0))
+        self.halfcourt_line()
+        self.display_score() 
+        pygame.display.flip()
+
+    def paddle(self, paddle_xy_pos, paddle_name:str):
+        paddle_size = (self.nce['paddle_x'], self.nce['paddle_y'])
+        paddle = pygame.Surface(paddle_size)
+        
+        new_pad_x = paddle_xy_pos[0]-int(0.5*paddle_size[0])
+        new_pad_y = paddle_xy_pos[1]-int(0.5*paddle_size[1])
+
+        # draws the black paddle to cover previous frame's paddle
+        paddle.fill((0, 0, 0))
+        if paddle_name == "a":
+            self.court.blit(paddle, (new_pad_x, self.prev_pad_a_y)) # "plugs hole" with black 
+        else:
+            self.court.blit(paddle, (new_pad_x, self.prev_pad_b_y)) # "plugs hole" with black 
+        #pygame.display.flip()
+
+        # repaints in case the black removed other things
+        self.halfcourt_line()
+        self.display_score() 
+
+        # draws the whit paddle, starting from up-left corner
+
+        paddle.fill((255,255,255))
+        self.court.blit(paddle, (new_pad_x, new_pad_y))
+        pygame.display.flip()
+
+        # save for next black painting
+        if paddle_name == "a":
+            self.prev_pad_a_y = new_pad_y
+        else:
+            self.prev_pad_b_y = new_pad_y
+
+    def ball(self, ball_size, ball_xy_pos):
+        ball = pygame.Surface((ball_size, ball_size))
+        
+        ball.fill((0, 0, 0)) # creates black ball to cover previous frame's ball
+        self.court.blit(ball, (self.prev_x, self.prev_y)) # "plugs hole" with black ball
+        self.halfcourt_line()
+        self.display_score() 
+
+        new_x = ball_xy_pos[0]-int(0.5*ball_size)
+        new_y = ball_xy_pos[1]-int(0.5*ball_size)
+        self.prev_x = new_x
+        self.prev_y = new_y     
+        #pygame.display.flip()
+
+        ball.fill((255,255,255))
+        self.court.blit(ball, (new_x, new_y))
+
+        pygame.display.flip()
+
+    def set_score(self, score_a, score_b):
+        self.score_a = score_a
+        self.score_b = score_b
+            
+    def display_score(self):
+        self.score(self.score_a, self.nce["ball_size"], "left", (self.nce["court_x"], self.nce["court_y"])) 
+        self.score(self.score_b, self.nce["ball_size"], "right", (self.nce["court_x"], self.nce["court_y"]))
+
+    def score(self, score:int, ball_size:int, side, screen_size):
+        if side == 'left':
+            offset_x = 0.33 * screen_size[0]
+            offset_y = 0.1 * screen_size[1] 
+        if side == 'right':
+            offset_x = 0.8 * screen_size[0]
+            offset_y = 0.1 * screen_size[1] 
+
+        units = score % 10
+        self.digit(Draw.digits[units], ball_size, (offset_x + ball_size, offset_y))
+        tens = int(score / 10)
+        if tens > 0:
+            self.digit(Draw.digits[tens], ball_size, (offset_x - 6 * ball_size, offset_y))
+        pygame.display.flip()
+
+    def digit(self, digit:tuple, ball_size:int, offset_xy):    
+        for i_line in range(len(digit)):
+            line = digit[i_line]
+            for i_column in range(len(line)):
+                column = line[i_column]
+                pixel = pygame.Surface((ball_size,ball_size))
+                if column == 1:
+                    pixel.fill((255,255,255))
+                if column == 0:
+                    pixel.fill((0,0,0))
+
+                self.court.blit(pixel, (offset_xy[0] + ball_size*i_column, offset_xy[1] + ball_size*i_line))
+
+
+#center
+                
+#paddle positions
+                
+#
+
+def calc_fps(x):
+    
+   return int(1000/x)
+
+
+
+
+def main(): 
+    non_changing_state = {
+        'court_x': 825,   # constants: 825x525 was original resolution of Pong
+        'court_y': 525, 
+        'paddle_x': 10,   # paddle width
+        'paddle_y': 50,   # paddle length  
+        'ball_size': 10 } 
+    frame = Frame(non_changing_state)
+    start_vel = (random.randint(10, 20), random.randint(10,15))
+    frame.init(
+        start_pos=(408,258), 
+        start_vel=start_vel 
+        )
+
+    running = True
+    
+    fps = calc_fps(60) # changes fps to desired amount
+    timer_event = pygame.USEREVENT + 1
+    pygame.time.set_timer(timer_event, fps)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:      # only deals with first instance of pressed button
+                if event.key == K_ESCAPE:
+                    running = False                
+                if event.key == K_RETURN:
+                    frame.play()
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == timer_event:
+                frame.tick()
+                keys = pygame.key.get_pressed()   # allows user to hold down button and still move
+                if keys[pygame.K_UP]:           #up-arrow
+                    frame.move_pad_b_up()
+                if keys[pygame.K_DOWN]:         #down-arrow
+                    frame.move_pad_b_down()
+                if keys[pygame.K_q]:            #q-key
+                    frame.move_pad_a_up()
+                if keys[pygame.K_a]:            #a-key
+                    frame.move_pad_a_down()          
+
+    frame.quit()
+
+if __name__ == "__main__":
+    main()
 
--- project.py
+++ project.py
@@ -1,60 +1,22 @@
-from frame import Frame
-import random
-import pygame
 
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_ESCAPE,
-    KEYDOWN,
-    K_RETURN,
-    K_q,
-    K_a,
-    QUIT,
-)
 
-def main(): 
-    non_changing_state = {
-        'court_x': 825,   # constants: 825x525 was original resolution of Pong
-        'court_y': 525, 
-        'paddle_x': 10,   # paddle width
-        'paddle_y': 50,   # paddle length  
-        'ball_size': 10 } 
-    frame = Frame(non_changing_state)
-    start_vel = (random.randint(10, 20), random.randint(10,15))
-    frame.init(
-        start_pos=(408,258), 
-        start_vel=start_vel 
-        )
+def main():
+    albums = {}
+    books = {}
+    movies = {}
 
-    running = True
     
-    time_delay = 50 
-    timer_event = pygame.USEREVENT + 1
-    pygame.time.set_timer(timer_event, time_delay)
+    catagory = input("Type database you would like to edit: | Albums | Books | Movies | wishlists | : ").strip()
+    item = input(f"Item to add to {catagory}: ")
+    
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:      # only deals with first instance of pressed button
-                if event.key == K_ESCAPE:
-                    running = False                
-                if event.key == K_RETURN:
-                    frame.play()
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == timer_event:
-                frame.tick()
-                keys = pygame.key.get_pressed()   # allows user to hold down button and still move
-                if keys[pygame.K_UP]:           #up-arrow
-                    frame.move_pad_b_up()
-                if keys[pygame.K_DOWN]:         #down-arrow
-                    frame.move_pad_b_down()
-                if keys[pygame.K_q]:            #q-key
-                    frame.move_pad_a_up()
-                if keys[pygame.K_a]:            #a-key
-                    frame.move_pad_a_down()          
 
-    frame.quit()
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
